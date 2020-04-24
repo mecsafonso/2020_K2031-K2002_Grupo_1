65f13cbb2f9e5a66f614031038bea0e912e4aaeb
erangelortega <elianarangelortega@gmail.com>
2020-04-24 15:51:39 -0300

    TP 1 - Correccion de funcion columnaSgt y matriz

--- TP_1/TP_1.c
+++ TP_1/TP_1.c
@@ -11,14 +11,13 @@
 #define Q5 5
 #define RECHAZO 6
 
-//variables costantes
+//Variables Cosntantes
 const char CENTINELA = ',';
 
 //FUNCIOINES
 void ejecutarAutomata (FILE*, FILE*);   //Ejecuta el automata y genera un txt de salida
 void estadoFinal (int, FILE*);          //Detecta en que estado se escuentra el automata y graba un mensaje en el archivo de salida dependiendo del mismo
 int columnaSgt (char);                  //Analiza a que columna de la matriz automata debe acceder
-int buscarCaracter (char[], char);      //Busca un caracter en una cadena
 
 
 int main (){
@@ -37,11 +36,11 @@ int main (){
 
 void ejecutarAutomata (FILE* original, FILE* arcFinal){
     int estadoActual = INICIO;
-    char simbolo;    
+    char caracter;    
 
     //Matriz del Automata
 
-    int automata [6] [6];
+    int automata [7] [6];
     automata [INICIO] [0] = Q2;
     automata [INICIO] [1] = Q1;
     automata [INICIO] [2] = Q1;
@@ -82,19 +81,25 @@ void ejecutarAutomata (FILE* original, FILE* arcFinal){
     automata [Q5] [2] = RECHAZO;
     automata [Q5] [3] = RECHAZO;
     automata [Q5] [4] = RECHAZO;
-    automata [Q5] [5] = RECHAZO;   
+    automata [Q5] [5] = RECHAZO; 
 
-    while ((simbolo = fgetc(original)) != EOF){
+    automata [RECHAZO] [0] = RECHAZO;
+    automata [RECHAZO] [1] = RECHAZO;
+    automata [RECHAZO] [2] = RECHAZO;
+    automata [RECHAZO] [3] = RECHAZO;
+    automata [RECHAZO] [4] = RECHAZO;
+    automata [RECHAZO] [5] = RECHAZO;  
 
-        if (simbolo == CENTINELA){
+    //Se evalúa cada palabra caracter a carater para definir un estadoFinal -> decimal, octal, hexadecimal o NO reconocido
+    while ((caracter = fgetc(original)) != EOF){
+
+        if (caracter == CENTINELA){
             estadoFinal (estadoActual, arcFinal);
             estadoActual = INICIO;
         }
-        else {
-            if(estadoActual != RECHAZO) {                
-            estadoActual = automata [estadoActual] [columnaSgt (simbolo)];
-            }
-            fprintf (arcFinal, "%c", simbolo);
+        else {               
+            estadoActual = automata [estadoActual] [columnaSgt (caracter)];
+            fprintf (arcFinal, "%c", caracter);
         }
     }
     estadoFinal (estadoActual, arcFinal);
@@ -102,9 +107,6 @@ void ejecutarAutomata (FILE* original, FILE* arcFinal){
 
 void estadoFinal (int estadoActual, FILE* arcFinal){
     switch (estadoActual){
-        case INICIO:
-            fprintf (arcFinal, "\t Espacio en blanco.\n");
-            break;
         case Q1:
             fprintf (arcFinal, "\t es un decimal.\n");
             break;
@@ -115,43 +117,31 @@ void estadoFinal (int estadoActual, FILE* arcFinal){
             fprintf (arcFinal, "\t es un octal.\n");
             break;
         default:
-            fprintf (arcFinal, "\t no reconocido.\n");
+            fprintf (arcFinal, "\t NO reconocido.\n");
             break;
     }
 }
 
-int columnaSgt (char simbolo){
-    //Alfabeto del automata
-    char limites[] = "0123456789xXabcdefABCDEF";
-    
-    switch ((int)buscarCaracter(limites, simbolo)) {
-    case 0:
+int columnaSgt (char caracter){   
+    if (caracter == '0') {
         return INICIO;
-        break;
-    case 1 ... 7:
-        return Q1;
-        break;
-    case 8 ... 9:
-        return Q2;
-        break;    
-    case 10 ... 11:
-        return Q3;
-        break;    
-    case 12 ... 23:
-        return Q4;
-        break;
-    default:
-        return Q5;
-        break;
     }
-}
-
-int buscarCaracter (char cadena[], char caracter){
-    int i = 0;
-    while ( cadena[i] != '\0' ) { 
-        if ( cadena[i] == caracter ) 
-            return i;
-        i++; 
-    }
-    return i;
+        else if  (caracter >= '1' && caracter <= '7'){
+            return Q1;
+        }
+        else if  (caracter == '8' || caracter == '9'){
+            return Q2;
+        }    
+        else if  (caracter == 'x' || caracter == 'X'){
+            return Q3;
+        }   
+        else if  (caracter >= 'a' && caracter <= 'f'){
+            return Q4;
+        }   
+        else if  (caracter >= 'A' && caracter <= 'F'){
+            return Q4;
+        }    
+        else {
+            return Q5;
+        }
 }
